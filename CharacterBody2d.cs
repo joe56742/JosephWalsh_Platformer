fc400425424acfd1dd6d7cf3cd70c1fd84ef9bb3
joe56742 <jwalsh2@ftstudent.org>
2026-09-16 13:58:32 -0400

    Expanded Map

--- CharacterBody2d.cs
+++ CharacterBody2d.cs
@@ -3,8 +3,13 @@ using System;
 
 public partial class CharacterBody2d : CharacterBody2D
 {
+	
+	
+	[Export]
+	public AnimatedSprite2D Sprite;
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+
 
 	public override void _PhysicsProcess(double delta)
 	{
